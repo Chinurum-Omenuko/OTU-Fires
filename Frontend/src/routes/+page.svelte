6b5d0bdcd8e7cada270959ecc6fc3c995f23d8fb
chinurum-omenuko <omen0003@algonquinlive.com>
2023-10-07 23:25:18 -0400

    changes

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<h1>News</h1>
-<p>News feed will go in here.
-    Created using an RSS Feed on local fires.
-</p>
+<div class="container">
+    <h1>News</h1>
+    <p>News feed will go in here.
+        Created using an RSS Feed on local fires.
+    </p>
+</div>
+
+<style>
+</style>
+
